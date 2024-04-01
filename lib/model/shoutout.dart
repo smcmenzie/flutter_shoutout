0d36239fcd9da71f61172d98290ce421a19a9e20
Simon McMenzie <simon@mcmenzie.com>
2024-04-01 18:37:49 +0100

    Added basic model and a home page

---
+++ lib/model/shoutout.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+
+
+class ShoutOut {
+  String message;
+  // todo:  Color messageColor;
+  // Color backgroundColor;
+  // DateTime dateCreated; 
+  // DateTime lastUpdated;
+  // bool isFavourite; // for future use
+  // bool canFlash;
+  // Text messageGroup; // might use in the future to group messages
+
+  ShoutOut(this.message);
+
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ShoutOuts', style: TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.bold,
+        ),),
+        // backgroundColor: Colors.brown[700],
+        centerTitle: false,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            color: Colors.green[200],
+            padding: const EdgeInsets.all(20),
+            child: const Text('Hello', textScaler: TextScaler.linear(2)),
+          ),
+          Container(
+            color: Colors.red[100],
+            padding: const EdgeInsets.all(20),
+            child: const Text('Goodbye', textScaler: TextScaler.linear(2)),
+          )
+        ]
+      ),
+      
+    );
+  }
+}
