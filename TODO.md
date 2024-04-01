eb353cb96faf59f7ed954192ca662a5060e1d82f
Simon McMenzie <simon@mcmenzie.com>
2024-04-01 18:51:01 +0100

    Quick commit

---
+++ TODO.md
@@ -0,0 +1,10 @@
+# ShoutOut TODO list
+
+- [ ] Create a basic model
+
+- [x] Save data to disk
+- [ ] 
+
+- [x] Write the press release
+- [ ] Update the website
+- [ ] Contact the media
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   const Home({super.key});
 
-  
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
